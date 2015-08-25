5202d99a581c5dbd402007b1f83e863e4b424e31
松島凛奈 <rinna@net72-dhcp43.sfc.keio.ac.jp>
2015-08-25 15:05:30 +0900

    first commit

--- Gacha/ResultViewController.swift
+++ Gacha/ResultViewController.swift
@@ -6,4 +6,88 @@
 //  Copyright (c) 2015年 Rinna Matsushima. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+   // @IBOutlet var haikeiImageView: UIImageView!
+ //   @IBOutlet var monsterImageView: UIImageView!
+    @IBOutlet var haikeiImageView: UIImageView!
+    @IBOutlet var monsterImageView: UIImageView!
+    
+    var monsterArray: [UIImage]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var number = Int(rand() % 9 + 1)
+        
+        monsterArray = [UIImage(named: "monster001.png")!,
+            UIImage(named: "monster002.png")!,
+            UIImage(named: "monster003.png")!,
+            UIImage(named: "monster004.png")!,
+            UIImage(named: "monster005.png")!,
+            UIImage(named: "monster006.png")!,
+            UIImage(named: "monster007.png")!,
+            UIImage(named: "monster008.png")!,
+            UIImage(named: "monster009.png")!,
+            UIImage(named: "monster010.png")!,
+        ]
+        
+        monsterImageView.image = monsterArray[number]
+   
+        if number == 9 {
+            
+            haikeiImageView.image = UIImage (named: "bg_gold");
+            
+        }
+        else if number == 8 || number == 7 {
+            
+            haikeiImageView.image = UIImage (named: "bg_red");
+            
+        }
+        else if number <= 6 {
+            
+            haikeiImageView.image = UIImage (named: "bg_blue");
+            
+        }
+
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        var animation = CABasicAnimation(keyPath: "transform")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func back() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
